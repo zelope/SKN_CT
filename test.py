43c01c5b6c905d4a189814c58f8542669f2e7128
zelope <lig980214@gmail.com>
2024-07-30 19:54:12 +0900

    modified:   CT/lecture_note.ipynb 	modified:   CT/probelm_solv/0730.ipynb 	modified:   test.py

--- test.py
+++ test.py
@@ -1,33 +1,22 @@
-import re
+import  sys
 
 
-def solve(expression:str):
-    m_ans = eval(expression)
-    #문자열을 수식 계산
+def solve():
+    n,m = map(int, sys.stdin.readline().split())
+    # n = 개수 m = 근점해야하는 값
+    n_list = list(map(int, sys.stdin.readline().split()))
+    answer = -1 
     
-    token = re.findall(r'\d+|\+|\-|\*|\/',expression)
-    # \d+ or + or - or * or / 과 매칭되는 것들을 찾아서 리스트로 반환
-    # \d+ 는 한개 이상의 숫자로 이루어진 것
-    print(token)
+    for i in range(n):
+        for j in range(i+1,n):
+            for k in range(j+1,n):
+                b_force = n_list[i]+n_list[j]+n_list[k]
+                
+                if (b_force <= m) and (b_force > answer):
+                    answer = b_force
     
-    f_ans = int(token[0])
-    for tag in range(1,len(token),2):
-        if token[tag] == "+":
-            f_ans += int(token[tag+1])
-        elif token[tag] == "-":
-            f_ans -= int(token[tag+1])
-        elif token[tag] == "*":
-            f_ans *= int(token[tag+1])
-        elif token[tag] == "/":
-            f_ans /= int(token[tag+1])
-        else:
-            pass
-    
-    return m_ans, f_ans
+    return answer
 
 if __name__ == "__main__":
     
-    male, female = solve("123+45*2-8/4")
-    
-    print(f'철수 : {male}')
-    print(f'영희 : {female}')
+    print(solve())
