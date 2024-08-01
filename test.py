36179cdfe7b64acabcb1157a8059023489dffa40
zelope <lig980214@gmail.com>
2024-08-01 21:14:39 +0900

    modified:   CT/probelm_solv/0730.ipynb 	modified:   CT/probelm_solv/0731.ipynb 	modified:   CT/probelm_solv/0801.ipynb 	modified:   test.py

--- test.py
+++ test.py
@@ -1,24 +1,22 @@
-import  sys
-
-
 def solve():
-    n = int(input())
-    size_list = list(map(int, sys.stdin.readline().split()))
-    t, p = map(int, sys.stdin.readline().split())
-    p_ans = n // p
-    p_rest = n % p
-    t_ans = 0
-    for size in size_list:
-        if size == 0:
-            continue
-        need = size // t
-        if size%t == 0:
-            t_ans += need
-        else:
-            t_ans += need+1
-    print(f'We need {t_ans} T-shirt bundle \n We need {p_ans} pencil bundle and {p_rest} pieces')
+    for i in range(3,0,-1):
+        x = input()
+        if x.isdigit():
+            number = int(x) + i
+    if number % 15 == 0:
+        answer = "FizzBuzz"
+    elif number % 3 == 0:
+        answer = "Fizz"
+    elif number % 5 ==0:
+        answer = "Buzz"
+    else:
+        answer = str(number)
+    
+    print(answer)
     return None
-
+        
+    
+    
 if __name__ == "__main__":
     
     solve()
