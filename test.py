754f18577f1301972cbae2f3a21c4d67972f0399
zelope <ljg980214@gmail.com>
2024-08-02 19:00:21 +0900

    0802_BJ18110

--- test.py
+++ test.py
@@ -1,18 +1,21 @@
 def solve():
-    for i in range(3,0,-1):
-        x = input()
-        if x.isdigit():
-            number = int(x) + i
-    if number % 15 == 0:
-        answer = "FizzBuzz"
-    elif number % 3 == 0:
-        answer = "Fizz"
-    elif number % 5 ==0:
-        answer = "Buzz"
-    else:
-        answer = str(number)
+    N = int(input())
     
+    #입력받고
+    score_list = [int(input()) for _ in range(N)]
+    #정렬하고
+    score_list.sort()
+    #위 아래 0.15씩 삭제하고
+    trim = round(N*0.15)
+    trimed_list = score_list[trim:-1*trim]
+    
+    #평균 구하고
+    avg = sum(trimed_list)/len(trimed_list)
+    
+    #평균도 반올림하고
+    answer = round(avg)
     print(answer)
+    
     return None
         
     
