ff14e58ed0c6cd786da171c088110a6801e6399f
zelope <ljg980214@gmail.com>
2024-08-04 02:24:44 +0900

    lecture_round&sort

--- test.py
+++ test.py
@@ -1,25 +1,26 @@
-def solve():
-    N = int(input())
+# def solve():
+#     N = int(input())
     
-    #입력받고
-    score_list = [int(input()) for _ in range(N)]
-    #정렬하고
-    score_list.sort()
-    #위 아래 0.15씩 삭제하고
-    trim = round(N*0.15)
-    trimed_list = score_list[trim:-1*trim]
+#     #입력받고
+#     score_list = [int(input()) for _ in range(N)]
+#     #정렬하고
+#     score_list.sort()
+#     #위 아래 0.15씩 삭제하고
+#     trim = round(N*0.15)
+#     trimed_list = score_list[trim:-1*trim]
     
-    #평균 구하고
-    avg = sum(trimed_list)/len(trimed_list)
+#     #평균 구하고
+#     avg = sum(trimed_list)/len(trimed_list)
     
-    #평균도 반올림하고
-    answer = round(avg)
-    print(answer)
+#     #평균도 반올림하고
+#     answer = round(avg)
+#     print(answer)
     
-    return None
+#     return None
         
     
     
 if __name__ == "__main__":
     
-    solve()
+    print(round(4.5))
+    print(round(4.55))
